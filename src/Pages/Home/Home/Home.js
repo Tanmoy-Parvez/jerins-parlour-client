5babf4ed7c70cc47c22796d6f47e892491649fe2
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-08 22:57:42 +0600

    subscribe added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Navigation from '../../Shared/Navigationbar/Navigation';
 import Banner from '../Banner/Banner';
 import MiddleBanner from '../MiddleBanner/MiddleBanner';
 import Services from '../Services/Services';
+import Subscribe from '../Subscribe/Subscribe';
 import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <MiddleBanner></MiddleBanner>
             <Testimonials></Testimonials>
+            <Subscribe></Subscribe>
 
         </div>
     );
---
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Subscribe = () => {
+    return (
+        <div className="banner-bg text-center py-5 my-5">
+            <div className="">
+                <h1 className="fw-bold">
+                    Let us handle your
+                </h1>
+                <h1 className="fw-bold">
+                    Project, <span className="pink-text">Personality</span>.
+                </h1>
+                <form action="" className="mt-5 w-50 mx-auto">
+                    <div className="row container mx-auto">
+                        <div className="col-md-6">
+                            <input type="text" className="form-control border-0 py-2" placeholder="First Name" />
+                        </div>
+                        <div className="col-md-6">
+                            <input type="text" className="form-control border-0 py-2" placeholder="Last Name" />
+                        </div>
+                    </div>
+                    <div className="row container mx-auto mt-3">
+                        <div className="col-md-6">
+                            <input type="email" className="form-control border-0 py-2" placeholder="Email Address" />
+                        </div>
+                        <div className="col-md-6">
+                            <input type="telephone" className="form-control border-0 py-2" placeholder="Phone Number" />
+                        </div>
+                    </div>
+                    <div className="row container mx-auto mt-3">
+                        <div className="col-md-12">
+                            <textarea rows="6" type="text" className="form-control border-0" placeholder="Your Message" />
+                        </div>
+
+                    </div>
+                </form>
+                <button type="submit" className="btn regular-btn py-2 mt-4">Send Message</button>
+            </div>
+        </div>
+    );
+};
+
+export default Subscribe;
